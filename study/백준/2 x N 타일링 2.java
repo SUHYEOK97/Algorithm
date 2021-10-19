7eea2ba2212d8f094fb437ddc6f83e35560333b1
김수혁 <56239516+SUHYEOK97@users.noreply.github.com>
2021-10-19 16:30:32 +0900

    Create 2 x N 타일링 2.java

---
+++ study/백준/2 x N 타일링 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		dp = new int[N+1];
+		dp[0] = 1; dp[1] = 1;
+		for(int i=2; i<=N; i++) dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+		System.out.println(dp[N]);
+	}
+}
